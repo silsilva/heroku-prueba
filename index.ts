a99f8b78b34a03c4c8504aab331a1a4c7c6fdb50
SIL_SILVA <silsilva.89@gmail.com>
2022-03-08 19:13:26 -0300

    env vars

--- index.ts
+++ index.ts
@@ -2,6 +2,23 @@ import * as  express from "express";
 const app = express();
 const port = process.env.PORT || 3000;
 
+
+//console.log(process.env.USER_FULLNAME);
+//LO DE ABAJO ES LO QUE ESCRIBO EN LA TERMINAL TAL CUAL
+//export USER_FULLNAME=silvi
+//$ echo $USER_FULLNAME
+// $ yarn dev
+// $ ts-node index.ts
+
+// console.log(process.env.NODE_ENV); 
+// console.log(process.env.DB_HOST); 
+
+
+app.get("/env", (req, res) => {
+    res.json({
+        environment: process.env.NODE_ENV,
+    });
+});
 app.get("/hola", (req, res) => {
     res.json({
         message: "hola soy el servidor, heroku",
